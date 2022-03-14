85e37cef941d45e986a1fec0147efbafce0ad7b6
jackholbrook <jholbrook@seattleu.edu>
2022-03-13 19:47:29 -0700

    jack commit added cmake build to server

---
+++ server/build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/st/jholbrook/milestone2.6/server/RPCImpl.cpp" "CMakeFiles/server.dir/RPCImpl.cpp.o" "gcc" "CMakeFiles/server.dir/RPCImpl.cpp.o.d"
+  "/home/st/jholbrook/milestone2.6/server/RPCServer.cpp" "CMakeFiles/server.dir/RPCServer.cpp.o" "gcc" "CMakeFiles/server.dir/RPCServer.cpp.o.d"
+  "/home/st/jholbrook/milestone2.6/server/main.cpp" "CMakeFiles/server.dir/main.cpp.o" "gcc" "CMakeFiles/server.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ server/build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/RPCImpl.cpp.o"
+  "CMakeFiles/server.dir/RPCImpl.cpp.o.d"
+  "CMakeFiles/server.dir/RPCServer.cpp.o"
+  "CMakeFiles/server.dir/RPCServer.cpp.o.d"
+  "CMakeFiles/server.dir/main.cpp.o"
+  "CMakeFiles/server.dir/main.cpp.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
